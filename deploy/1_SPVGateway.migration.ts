0e8ce83612f2d45a51706f1d281e4542e91c8d90
Oleh Komendant <oleg02kom@gmail.com>
2025-09-01 21:03:22 +0300

    Add deterministic deployment

--- deploy/1_SPVGateway.migration.ts
+++ deploy/1_SPVGateway.migration.ts
@@ -1,19 +1,27 @@
 import { Deployer, Reporter } from "@solarity/hardhat-migrate";
 
-import { SPVGateway__factory } from "@ethers-v6";
+import { SPVGateway__factory, ICreateX__factory } from "@ethers-v6";
 
 import { getConfig } from "./config/config";
+import { getSPVGatewayAddr, getSPVGatewaySalt } from "./helpers/helpers";
 
 export = async (deployer: Deployer) => {
-  const config = (await getConfig())!;
+  const config = await getConfig();
 
-  const spvGateway = await deployer.deploy(SPVGateway__factory);
+  const createXDeployer = await deployer.deployed(ICreateX__factory, "0xba5Ed099633D3B313e4D5F7bdc1305d3c28ba5Ed");
 
-  await spvGateway["__SPVGateway_init(bytes,uint256,uint256)"](
-    config.blockHeader,
-    config.blockHeight,
-    config.cumulativeWork,
+  const salt = getSPVGatewaySalt();
+  const initCalldata = SPVGateway__factory.createInterface().encodeFunctionData(
+    "__SPVGateway_init(bytes,uint64,uint256)",
+    [config.blockHeader, config.blockHeight, config.cumulativeWork],
   );
 
-  Reporter.reportContracts(["SPVGateway", await spvGateway.getAddress()]);
+  await createXDeployer.deployCreate2AndInit(salt, SPVGateway__factory.bytecode, initCalldata, {
+    constructorAmount: 0n,
+    initCallAmount: 0n,
+  });
+
+  const spvGatewayAddr = await getSPVGatewayAddr(createXDeployer);
+
+  Reporter.reportContracts(["SPVGateway", spvGatewayAddr]);
 };
